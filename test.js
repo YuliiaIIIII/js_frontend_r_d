c08b400f5b147b3a055a30f766e6f3e468e6c559
YuliiaIIIII <yulyamashka@gmail.com>
2023-02-21 18:36:36 +0100

    Home work. Lesson 21

---
+++ test.js
@@ -0,0 +1,10 @@
+let firstValue = prompt("Введіть перше число", "");
+
+let secondValue = prompt("Введіть друге число", "");
+
+let addResult = firstValue + secondValue;
+let subResult = firstValue - secondValue;
+let multipleResult = firstValue * secondValue;
+let divResult = firstValue / secondValue;
+
+alert ("Результати математичних операцій: \n додавання - " + addResult + " \n віднімання - " + subResult + "\n множення - " + multipleResult + "\n ділення - " + divResult);
