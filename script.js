8669998d524ab790a1a91d2e2a9b7b41cc01589f
YuliiaIIIII <yulyamashka@gmail.com>
2023-02-25 16:41:56 +0100

    Home work. Lesson 22

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 let firstValue = prompt("Введіть перше число", "");
-
 let secondValue = prompt("Введіть друге число", "");
 
-let addResult = Number(firstValue) + Number(secondValue);
-let subResult = firstValue - secondValue;
-let multipleResult = firstValue * secondValue;
-let divResult = firstValue / secondValue;
+let addResult = 0;
+let subResult = 0;
+let multipleResult = 0;
+let divResult =  0;
 
-alert (`Результати математичних операцій.
-Додавання: ${addResult}
-Віднімання: ${subResult}
-Множення: ${multipleResult}
-Ділення: ${divResult}`);
+addResult = Number(firstValue) + Number(secondValue);
+subResult = firstValue - secondValue;
+multipleResult = firstValue * secondValue;
+divResult = firstValue / secondValue;
+
+if(firstValue == "" || secondValue == ""){
+    alert(`Error. Обрахунок неможливий`);
+}
+else if(firstValue < secondValue){
+    confirm(`Ви впевнені, що хочете продовжити операцію?`);
+    if(true){
+        alert (`Результат додавання: ${addResult}`);
+        alert (`Результат віднімання: ${subResult}`);
+        alert (`Результат множення: ${multipleResult}`);
+        alert (`Результат ділення: ${divResult}`);
+    }
+    // else{
+    //     alert (`Результат додавання: ${addResult}`);
+    //     alert (`Результат множення: ${multipleResult}`);
+    //     alert (`Результат ділення: ${divResult}`);
+    // }
+
+    
+//   Після підтвердження – показати результат. віднімання, в противному випадку пропустити повідомлення з показом результату
+
+if(secondValue == 0){
+    alert (`Error. На нуль ділити не можна!`);
+}else{
+   
+    alert (`Результат ділення: ${divResult}`);
+}
+}
