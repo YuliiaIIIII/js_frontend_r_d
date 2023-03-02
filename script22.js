8e50ae3713639bcfb1f744fa47430b7b0a7d30ee
YuliiaIIIII <yulyamashka@gmail.com>
2023-03-02 03:45:16 +0200

    Home work. Lesson 22

--- script22.js
+++ script22.js
@@ -1,48 +1,33 @@
 let firstValue = prompt("Введіть перше число", "");
 let secondValue = prompt("Введіть друге число", "");
 
-// let addResult = 0;
-// let subResult = 0;
-// let multipleResult = 0;
-// let divResult =  0;
-
 let addResult = Number(firstValue) + Number(secondValue);
 let subResult = firstValue - secondValue;
 let multipleResult = firstValue * secondValue;
 let divResult = firstValue / secondValue;
+if(secondValue == 0){
+    divResult = `Error. На нуль ділити не можна!`;
+}
 
 if(firstValue == "" || secondValue == ""){
     alert(`Error. Обрахунок неможливий`);
 }
+
 else if(firstValue < secondValue){
-    confirm(`Ви впевнені, що хочете продовжити операцію?`);
+    let userChoise = confirm(`Ви впевнені, що хочете продовжити операцію?`);
+    if(userChoise){
+        alert (`Результат додавання: ${addResult}`);
+        alert (`Результат віднімання: ${subResult}`);
+        alert (`Результат множення: ${multipleResult}`);
+        alert (`Результат ділення: ${divResult}`);
+    }else{
+        alert (`Результат додавання: ${addResult}`);
+        alert (`Результат множення: ${multipleResult}`);
+        alert (`Результат ділення: ${divResult}`);
+    }
 }else{
     alert (`Результат додавання: ${addResult}`);
     alert (`Результат віднімання: ${subResult}`);
     alert (`Результат множення: ${multipleResult}`);
-
-        if(secondValue == 0){
-            alert (`Error. На нуль ділити не можна!`);
-        }else{
-            alert (`Результат ділення: ${divResult}`);
-        }
-    
+    alert (`Результат ділення: ${divResult}`);
 }
-
-// if(confirm ("Ok")== true){
-
-// }else{
-// alert (`Результат додавання: ${addResult}`);
-
-// alert (`Результат множення: ${multipleResult}`);
-// }
-    // if{
-    //     alert (`Результат додавання: ${addResult}`);
-    //     alert (`Результат множення: ${multipleResult}`);
-    //     alert (`Результат ділення: ${divResult}`);
-    // }
-
-    
-//   Після підтвердження – показати результат. віднімання, в противному випадку пропустити повідомлення з показом результату
-
-
