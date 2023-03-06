a9fdf5b689684b2b59490475e049af18cb231ee5
YuliiaIIIII <yulyamashka@gmail.com>
2023-03-06 02:57:58 +0200

    Loops and arrays. Lesson 23. Checking of element types added

--- script23.js
+++ script23.js
@@ -1,33 +1,38 @@
 // 1. Створити новий масив на 10 елементів з довільними числами. 
-const newArray = [-10, 35, 20, 2, 0, 85, 77, 41, 45, 54];
-// const newArray = [10, 15, "test", 20, null, true, 25, 30, 35, 40, 45, 50, 55];
-
+// const newArray = [-10, 35, 20, 2, 0, 85, 77, 41, 45, 54];
+const newArray = ["test", -10, 5, "test2", 0, 75, null, true, 3, 45];
 
 // 2. Написати скрипт для пошуку суми елементів масиву
-// 3. Написати скрипт, який буде шукати мінімальне та максимальне значення елементів масиву.
-// 4. В пунктах 2 та 3 потрібно робити перевірку на тип елементу масиву і не брати до уваги елементи які не є числами
 let sum = 0;
-let minValue = newArray[0];
-let maxValue = newArray[0];
 for (let i = 0; i < newArray.length; i++){
     if(typeof newArray[i] == 'number'){  
     sum = sum + newArray[i];
     }
-    if (newArray[i] > maxValue) {
-        maxValue = newArray[i];
-    }
-    if (newArray[i] < minValue) {
-        minValue = newArray[i];
-    }
 }
 
-alert (`Сума всіх елементів: ` + sum);
-alert (`Найбільше значення: ` + maxValue);
-alert (`Найменше значення: ` + minValue);
-
+// 3. Написати скрипт, який буде шукати мінімальне та максимальне значення елементів масиву.
+// 4. В пунктах 2 та 3 потрібно робити перевірку на тип елементу масиву і не брати до уваги елементи які не є числами
+let minValue = Infinity;
+let maxValue = -Infinity;
+for (let i = 0; i < newArray.length; i++) {
+    if(typeof newArray[i] !== 'number'){
+        continue;
+    }else{
+        
+    if (maxValue < newArray[i]) {
+        maxValue = newArray[i];
+    }
+    if (minValue > newArray[i]) {
+        minValue = newArray[i];
+    }
+  } 
+}
+alert (`Сума всіх елементів: ${sum}`);
+alert (`Найбільше значення: ${maxValue}`);
+alert (`Найменше значення: ${minValue}`);
+    
 
 // 5. Написати скрипт який виведе наступне:
-
 // #
 // ##
 // ###
