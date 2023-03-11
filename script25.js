052024221fcca14ca598266802582a36c731f8aa
YuliiaIIIII <yulyamashka@gmail.com>
2023-03-11 23:50:12 +0200

    Lesson 25. Objects

--- script25.js
+++ script25.js
@@ -1,4 +1,5 @@
 // 1. Напишіть функцію, яка буде перевіряти обʼєкт на пустоту
+
 const testObject = {};
 const notEmptyObj = {
     name: "Sem",
@@ -14,6 +15,7 @@ console.log(checkEmptyObj(notEmptyObj));
 
 
 // 2. Опишіть обʼєкт user з полями name, age та методом sayHello, результатом виклику якого має бути повідомлення “Привіт, я name, мені age років”
+
 let user = {
     name: "James",
     age: 25,
@@ -23,23 +25,25 @@ let user = {
 }
 user.sayHello();
 
+
 // 3. Напишіть обʼєкт калькулятор. Він має містити метод ask, sum та mul. При виклику методу ask обʼєкт має запросити 2 числа та зберегти їх, як свої властивості. При виклику sum - вивести суму збережених чисел, при виклику mul - додаток чисел.
+
 let calc = {
+    firstValue: 0,
+    secondValue: 0,
     ask: function (){
-        let first = prompt("Введіть перше число: ");
-        let second = prompt("Введіть друге число: ");
-        
+        this.firstValue = parseFloat(prompt("Введіть перше число: "));
+        this.secondValue = parseFloat(prompt("Введіть друге число: ")); 
     },
     sum: function(){
-        let sumResult = first + second;
+        let sumResult = this.firstValue + this.secondValue;
         return `Сума чисел: ${sumResult}`;
     },
     mul: function(){
-        let mulResult = first * second;
+        let mulResult = this.firstValue * this.secondValue;
         return `Добуток чисел: ${mulResult}`;
     }
 }
 calc.ask();
 console.log(calc.sum());
 console.log(calc.mul());
-console.log(first, second);
