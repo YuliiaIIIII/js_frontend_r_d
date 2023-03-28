4b595eca473d2d6f25c2d4f7b603d4e9397122c9
YuliiaIIIII <yulyamashka@gmail.com>
2023-03-29 00:50:42 +0200

    function constructor

--- script26.js
+++ script26.js
@@ -1,22 +1,41 @@
 // 1. Створити конструктор Accumulator. Даний конструктор має приймати число, яке буде початковим значенням. Клас має реалізувати методи increment, при виклику якого передане значення має збільшуватись на 1, та decrement, при виклиці якого значення має зменшуватись.
 function Accumulator(value){
     this.value = value;
+    if(typeof value !== "number"){
+        console.log("Only for numbers");
+        return;
+    }
+    
     Accumulator.prototype.increment = function(){
-        value++;
-        console.log(value);
+        return `${++value}`;
     }
     Accumulator.prototype.decrement = function(){
-        value--;
-        console.log(value);
+        return `${--value}`;
     }
 }
-let userValue = new Accumulator(10);
-userValue.increment();
-userValue.decrement();
-userValue.increment();
-userValue.decrement();
+let userValue = new Accumulator(0);
+console.log(userValue.increment());
+console.log(userValue.increment());
+console.log(userValue.decrement());
+console.log(userValue.decrement());
+
 
 // 2. Створити конструктор CancelableAccumulator, який буде наслідувати Accumulator та додатково буде реалізовувати метод clear, при виклику якого значення збереженого числа має стати таким, яким його передали в конструкторі.
-function CancelableAccumulator(){
+function CancelableAccumulator(value){
+    Accumulator.call(this, value);
+    this.value = value;
 
+    CancelableAccumulator.prototype.clear = function() {
+        this.value = value;
 }
+}   
+
+  
+CancelableAccumulator.prototype = Object.create(Accumulator.prototype);
+let skipValue = new CancelableAccumulator(10);
+console.log(skipValue.value);
+console.log(skipValue.increment());
+skipValue.clear();
+console.log(skipValue.increment());
+console.log(skipValue.decrement());
+console.log(skipValue.decrement());
