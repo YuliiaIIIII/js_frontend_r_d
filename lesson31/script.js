fb40f6c6382b45b347a5e80c024c1f96af2fd9fa
YuliiaIIIII <yulyamashka@gmail.com>
2023-04-17 20:11:20 +0200

    Timer created

--- lesson31/script.js
+++ lesson31/script.js
@@ -1,31 +1,36 @@
-// window onload(){
+let timer = document.querySelector('.timer');
+timer.setAttribute('style','margin: 0 auto; background-color: lightblue; border: 2px dashed white; color: darkblue; font-family:Snell Roundhand, cursive; font-size: 26px; text-align: center');
 
-// }
-const today = new Date();
-console.log(today);
+const dayToMs = 86400000;
+const hourToMs = 3600000;
+const minuteToMs = 60000;
+const secToMs = 1000;
 
-const newYearDay = new Date(2023, 11, 31, 23, 59, 59);
-console.log(newYearDay);
+let NYDate = new Date(2024,0,1).getTime();
+// let NYDate = new Date(2023,03,17,19,52,0).getTime();
 
-let res = newYearDay - today;
-console.log(res);
+const interval =  setInterval(() => {
+    let todayDate = Date.now();
+    let difference = NYDate - todayDate;
+    // console.log(difference);
 
-function getData (dataValue){
-    dataValue.toISOString();
-    let yearValue = dataValue.getFullYear();
-    let monthValue = dataValue.getMonth();
-    let dayValue = dataValue.getDay();
-    
-    let hoursValue = dataValue.getHours();
-    let minutesValue = dataValue.getMinutes();
+    let days = Math.floor(difference / dayToMs);
+    let hours = Math.floor((difference % dayToMs) / hourToMs);
+    let minutes = Math.floor((difference % hourToMs) / minuteToMs);
+    let seconds = Math.floor((difference % minuteToMs) / secToMs);
 
-    console.log(yearValue);
-    console.log(monthValue);
-    console.log(dayValue);
-    
-    console.log(hoursValue);
-    console.log(minutesValue);
-}
-getData (today);
-getData (newYearDay);
+    let daysLeft = document.getElementById('daysLeft');
+    daysLeft.innerHTML = days;
+    let hoursLeft = document.getElementById('hoursLeft');
+    hoursLeft.innerHTML = hours;
+    let minutesLeft = document.getElementById('minutesLeft');
+    minutesLeft.innerHTML = minutes;
+    let secondsLeft = document.getElementById('secondsLeft');
+    secondsLeft.innerHTML = seconds;
 
+if(difference<=0){
+    clearInterval(interval);
+    alert (`Happy New Year!!!`);
+    }
+
+}, 1000);
