55f38b63701f7cc09527dedb8ddf2c27837366c2
YuliiaIIIII <yulyamashka@gmail.com>
2023-04-26 16:16:08 +0200

    Modified functions from the lesson24. Added color settings

--- script27.js
+++ script27.js
@@ -1,57 +1,106 @@
 // 1. Модифікувати функції з завдання 24 так, щоб результат дії цих функцій показувався на сторінці замість діалогового вікна.
+// 2. Якщо результат операції з завдання 1 буде відʼємним – показувати його червоним кольором, якщо додатнім – зеленим.
 
+let result = document.querySelector(".result");
+result.setAttribute('style','font-size: 20px; text-align: center');
 let firstValue = prompt("Введіть перше число");
 let secondValue = prompt("Введіть друге число");
-if(firstValue == "" || secondValue == ""){
-    alert(`Error. Обрахунок неможливий`);
+
+function setColor(value,spanElement){
+    if(value < 0){
+        spanElement.setAttribute('style','color: red');
+    }else if(value === 0){
+        spanElement.setAttribute('style','color: black');
+    }else{
+        spanElement.setAttribute('style','color: green');
+    }
+}
+
+function append(pElement, spanEl, resultEl){
+    spanEl.textContent = (`${resultEl}`);
+    setColor(resultEl,spanEl);
+    pElement.appendChild(spanEl); 
 }
 
 function getSum (first, second){
-    if(isNaN(first) || isNaN(second)){
-        return alert(`First or second value is not a number!`);
-    }else{
-        let addResult = parseFloat(first) + parseFloat(second);
-        console.log(`Результат додавання: ${addResult}`);
-    }
+    let addResult = parseFloat(first) + parseFloat(second);
+    let sumP = document.createElement("p");
+    sumP.textContent = (`Результат додавання: `);
+    result.appendChild(sumP);
+    let sumSpan = document.createElement("span");
+    append(sumP, sumSpan, addResult);
 }
-getSum(firstValue, secondValue);
 
 function getSub (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
-    }else{
     let subResult = first - second;
-    console.log(`Результат віднімання: ${subResult}`);
-    }
+    let subP = document.createElement("p");
+    subP.textContent = (`Результат віднімання: `);
+    result.appendChild(subP);
+    let subSpan = document.createElement("span");
+    append(subP, subSpan, subResult);
 }
-getSub(firstValue, secondValue);
 
 function getMult (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
-    }else{
     let multipleResult = first * second;
-    console.log(`Результат множення: ${multipleResult}`);
-    }
+    let multP = document.createElement("p");
+    multP.textContent = (`Результат множення: `);
+    result.appendChild(multP);
+    let multSpan = document.createElement("span");
+    append(multP, multSpan, multipleResult);
 }
-getMult(firstValue, secondValue);
 
 function getDiv (first, second){
-    if(isNaN(first, second)){
-        console.log(`First or second value is not a number!`);
+    let divResult = (first / second).toFixed(4);
+        if(secondValue == 0){
+            divResult = `Error. На нуль ділити не можна!`;
+        }
+    let divP = document.createElement("p");
+    divP.textContent = (`Результат ділення: `);
+    result.appendChild(divP);
+    let divSpan = document.createElement("span");
+    append(divP, divSpan, divResult);
+}
+
+function checkIfNumber (first, second){
+    if(first == "" || second == ""){
+        alert(`Error. Обрахунок неможливий`);
+        return;
+    }else if(isNaN(first) && isNaN(second)){
+        alert(`First and second value is not a number!`);
+        return;
+    }else if(isNaN(first)){
+        alert(`First value is not a number!`);
+        return;
+    }else if(isNaN(second)){
+        alert(`Second value is not a number!`);
+        return;
     }else{
-    let divResult = first / second;
-    if(secondValue == 0){
-        divResult = `Error. На нуль ділити не можна!`;
-    }
-    console.log(`Результат ділення: ${divResult}`);
+        getSum(firstValue, secondValue);
+        getSub(firstValue, secondValue);
+        getMult(firstValue, secondValue);
+        getDiv(firstValue, secondValue);
     }
 }
-getDiv(firstValue, secondValue);
+checkIfNumber (firstValue, secondValue)
 
 
-// 2. Зробити функції для пошуку мінімального і максимального числа в масиві.
-let testArray = [4, "testText", 7, -1, 0, null, 90, 35, true, 15, -8];
+// 3. Модифікувати скрипт до завдання 2 таким чином, щоб чим більшим (меншим) є результат – тим темнішим є колір.
+
+let array = document.querySelector(".array");
+array.setAttribute('style','margin-top: 50ps; font-size: 18px; text-align: center');
+let testArray = [12, "testText", 21, 55, 405, null, 190, 600, true];
+
+function colorSettings(value,element){
+    if(value < -1000){element.setAttribute('style','color: #480000');}
+    else if(value < -100){element.setAttribute('style','color: #780000');}
+    else if(value < -10){element.setAttribute('style','color:  #A80000');}
+    else if(value < 0){element.setAttribute('style','color: #C80000');}
+    else if(value === 0){element.setAttribute('style','color: #FF0000');}
+    else if(value < 10){element.setAttribute('style','color: #ff3333');}
+    else if(value < 100){element.setAttribute('style','color: #ff6666');}
+    else if(value < 1000){element.setAttribute('style','color: #ff9999');}
+    else if(value > 1000){element.setAttribute('style','color: #ffcccc');}
+}
 
 function minValueFunc(arr){
 let minValue = Infinity;
@@ -64,7 +113,11 @@ for (let i = 0; i < arr.length; i++) {
     }
   } 
 }
-console.log (`Найменше значення: ${minValue}`); 
+let stringResult = `Найменше значення: ${minValue}`;
+let p = document.createElement('p');
+p.textContent = stringResult;
+colorSettings(minValue,p);
+array.appendChild(p);
 }
 minValueFunc(testArray);
 
@@ -79,9 +132,10 @@ for (let j = 0; j < arr.length; j++) {
     }
   } 
 }
-console.log(`Найбільше значення: ${maxValue}`);
+let stringResult = `Найбільше значення: ${maxValue}`;
+let p = document.createElement('p');
+p.textContent = stringResult;
+colorSettings(maxValue,p);
+array.appendChild(p);
 }
 maxValueFunc(testArray);
-// 2. Якщо результат операції з завдання 1 буде відʼємним – показувати його червоним кольором, якщо додатнім – зеленим.
-
-// 3. Модифікувати скрипт до завдання 2 таким чином, щоб чим більшим (меншим) є результат – тим темнішим є колір.
